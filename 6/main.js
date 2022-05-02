f2645a6992cb3b32e707104dbb473c80063f2bca
Chr1skyy <tofucek@gmail.com>
2022-05-02 13:14:17 +0200

    weird

--- 6/main.js
+++ 6/main.js
@@ -5,7 +5,6 @@ const timeEl = document.querySelector('.time');
 const clock = document.querySelector('.clock');
 
 function czas() {
-  console.log(clock);
   clock.style.display = "flex";
 	const d = new Date();
 	let month = d.getMonth();
